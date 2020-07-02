6b3f7f389efee5fce241be6d8d5f69aefdffd80c
Richard Todd <rwtodd@users.noreply.github.com>
2020-07-02 18:49:56 -0500

    Converted macsrc itself to lua.
    It now runs well enough to pass the same tests the perl version
    passed.  Obviously the code fragments in the tests were also
    converted to lua.
    
    Next steps... (1) clean up the code (2) investigate whether
    templating makes sense or if code blocks are sufficient.

--- tests/compare.sh
+++ tests/compare.sh
@@ -2,6 +2,6 @@
 
 for num in $(seq -w 004)
 do
-	perl macsrc "${srcdir}/tests/tst$num.msrc" | diff --from-file="${srcdir}/tests/ans$num.txt" - || exit 1
+	lua macsrc < "${srcdir}/tests/tst$num.msrc" | diff --from-file="${srcdir}/tests/ans$num.txt" - || exit 1
 done
 
