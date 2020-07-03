474c872e280173cb79c48abdfc945bd48ae65d28
Richard Todd <rwtodd@users.noreply.github.com>
2020-07-02 22:11:32 -0500

    Created sandbox for code.
    Now, the user code in the files has no access to the `os` package,
    and can't use the global `print,` so it would be hard to damage
    the system.

--- tests/compare.sh
+++ tests/compare.sh
@@ -2,6 +2,8 @@
 
 for num in $(seq -w 006)
 do
-	lua macsrc < "${srcdir}/tests/tst$num.msrc" | diff --from-file="${srcdir}/tests/ans$num.txt" - || exit 1
+	lua macsrc < "${srcdir}/tests/tst$num.msrc" | diff --from-file="${srcdir}/tests/ans$num.txt" - \
+	&& echo "Test $num OK!" \
+	|| exit 1
 done
 
