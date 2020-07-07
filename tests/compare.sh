25fa0eb1eb18341a8dc630e11a95406c159b7805
Richard Todd <rwtodd@users.noreply.github.com>
2020-07-07 17:04:18 -0500

    Fragments defining fragments, and code visibility features.
    For example:
    
    @./define constant (name, value) /
    add_fragment(name, { value.." --[["..name.."]]" })
    @.
    
    ...gets used like so:  @.(define constant ('PI',3.14159))
    .. and thereafter in the file, @.(PI) works.  See tst007.msrc
    
    Also, you can now define fragments with extra *'s ~'s or ='s after
    the begin/end markers, to help them stand out in the source.  Like
    this:
    
    @. / fragment name / *****************
    contents of fragment
    are here
    @. ***********************************

--- tests/compare.sh
+++ tests/compare.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for num in $(seq -w 006)
+for num in $(seq -w 007)
 do
 	lua macsrc < "${srcdir}/tests/tst$num.msrc" | diff --from-file="${srcdir}/tests/ans$num.txt" - \
 	&& echo "Test $num OK!" \
