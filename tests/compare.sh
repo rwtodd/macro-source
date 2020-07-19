1c7f011b6f36ce0bef61f87f75526fc4f17c0d03
Richard Todd <rwtodd@users.noreply.github.com>
2020-07-19 14:21:01 -0500

    Added an ability to include a secondary file in the output.
    Also, improved handling of empty input, and renamed
    add_fragment to add_macro for enhanced clarity.

--- tests/compare.sh
+++ tests/compare.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-for num in $(seq -w 007)
+echo 'hello' > example.text
+
+for num in $(seq -w 009)
 do
 	./macsrc < "${srcdir}/tests/tst$num.msrc" | diff --from-file="${srcdir}/tests/ans$num.txt" - \
 	&& echo "Test $num OK!" \
