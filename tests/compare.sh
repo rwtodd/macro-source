1277be9acb7b821bd87934cbe92972f189fbb8b3
Richard Todd <rwtodd@users.noreply.github.com>
2020-08-18 12:16:31 -0500

    added ability to escape newlines in msrc source

--- tests/compare.sh
+++ tests/compare.sh
@@ -2,7 +2,7 @@
 
 echo 'hello' > example.text
 
-for num in $(seq -w 009)
+for num in $(seq -w 010)
 do
 	./macsrc < "${srcdir}/tests/tst$num.msrc" | diff --from-file="${srcdir}/tests/ans$num.txt" - \
 	&& echo "Test $num OK!" \
